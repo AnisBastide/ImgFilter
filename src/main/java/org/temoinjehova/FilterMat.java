c90ed8e9aa8c95a6d9e843767e29fa9305cbd511
Theovady Moutty <theomoutty@gmail.com>
2019-12-18 12:02:13 +0100

    added parent class in the comit i don't save is bonne class

---
+++ src/main/java/org/temoinjehova/FilterMat.java
@@ -0,0 +1,9 @@
+package org.temoinjehova;
+
+
+import org.bytedeco.opencv.opencv_core.Mat;
+
+public class FilterMat {
+    public Mat resultimpg;
+    public int size;
+}
--- src/main/java/org/temoinjehova/Main.java
+++ src/main/java/org/temoinjehova/Main.java
@@ -2,6 +2,7 @@ package org.temoinjehova;
 
 public class Main {
     public static void main(String[] args) {
-        
+        JavaCVHelper helper = new JavaCVHelper();
+        helper.exempleBlurFile();
     }
 }
