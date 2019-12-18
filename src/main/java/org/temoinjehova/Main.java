e2b15a60d065ba22fc33fa11a8717305e1da461d
Anis <anis.bastide@gmail.com>
2019-12-18 11:47:23 +0100

    added JavaCvHelper

--- src/main/java/org/temoinjehova/Main.java
+++ src/main/java/org/temoinjehova/Main.java
@@ -2,6 +2,8 @@ package org.temoinjehova;
 
 public class Main {
     public static void main(String[] args) {
+        JavaCVHelper helper = new JavaCVHelper();
+        helper.exempleBlurFile();
         
     }
 }
