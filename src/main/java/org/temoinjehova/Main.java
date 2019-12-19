2b4d7d7694eb59a322c818bd9038f752499800db
Anis <anis.bastide@gmail.com>
2019-12-19 15:33:08 +0100

    Added Filter selection and help in CLI

--- src/main/java/org/temoinjehova/Main.java
+++ src/main/java/org/temoinjehova/Main.java
@@ -1,9 +1,6 @@
 package org.temoinjehova;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.Options;
+import org.apache.commons.cli.*;
 
 import java.text.ParseException;
 import java.util.Arrays;
@@ -14,17 +11,20 @@ public class Main {
         String input="imgs";
         String output="output";
 
-        //String intputDirArg="";
-        String filterArg="";
-
         Options options= new Options();
         options.addOption("i","input-dir",true,"input directory");
         options.addOption("o","output-dir",true,"Output directory");
         options.addOption("h","help",false,"Display all arguments");
+        options.addOption("f","filter",true,"Choose filter");
 
+ 
         CommandLineParser parser = new DefaultParser();
         try{
             CommandLine cmd = parser.parse(options,args);
+            if (cmd.hasOption("h")){
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp( "ant", options );
+            }
             if(cmd.hasOption("i")){
                 input=cmd.getOptionValue("i");
                 System.out.println(input);
@@ -36,13 +36,38 @@ public class Main {
         } catch (org.apache.commons.cli.ParseException e) {
             e.printStackTrace();
         }
+        try{
+            CommandLine cmd = parser.parse(options,args);
+            if(cmd.hasOption("f")){
+               String filter=cmd.getOptionValue("f");
+                switch(filter){
+                    case "blur":BlurFilter blur = new BlurFilter();
+                    blur.process(input,output);
+                    break;
+                    case "dilate":Dilate dilate=new Dilate();
+                        dilate.process(input,output);
+                        break;
+                    case "greyscale": GreyScale grey=new GreyScale();
+                              grey.process(input,output);
+                              break;
+                    default:
+                        System.out.println("You need to give a filter");
+                    break;
+                }
 
-//        BlurFilter blur = new BlurFilter();
-//       blur.process(input,output);
-     Dilate dilate=new Dilate();
-      dilate.process(input,output);
- //       GreyScale grey=new GreyScale();
- //         grey.process(input,output);
+            }
+            if(cmd.hasOption("o")){
+                output = cmd.getOptionValue("o");
+                System.out.println(output);
+            }
+        } catch (org.apache.commons.cli.ParseException e) {
+            e.printStackTrace();
+        }
+
+
+//
+
+ //
 
     }
 }
